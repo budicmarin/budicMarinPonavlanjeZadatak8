e71cc37e416efe38b31f2fd7eaeafb2d5c207ff5
budicmarin <55134480+budicmarin@users.noreply.github.com>
2020-11-12 18:04:06 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace budicMarinPonavlanjeZadatak8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sport Nogomet = new Sport("Nogomet",true,10);
+            Sport Nogomet = new Sport("Vaterpolo", true, 10);
+            Sport Nogomet = new Sport("Curling", false, 10);
+        }
+    }
+}
---
+++ Sport.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace budicMarinPonavlanjeZadatak8
+{
+    class Sport
+    {
+        private string Naziv;
+        private bool IgraLiSeLoptom;
+        private byte BrojIgraca;
+        public Sport(string naziv)
+        {
+            Naziv = naziv;
+        }
+        public Sport(string naziv,bool igra)
+        {
+            Naziv = naziv;
+            IgraLiSeLoptom = igra;
+        }
+        public Sport(string naziv, bool igra,byte broj)
+        {
+            Naziv = naziv;
+            IgraLiSeLoptom = igra;
+            BrojIgraca = broj;
+        }
+    }
+}
